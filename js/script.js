4b2d576125cbe7df5306c7d5d92a7f0b2020f34a
SamuFachaPro <samuelforero367@gmail.com>
2026-03-10 14:33:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-AOS.init({
-duration:1200,
-offset:100,
-once:true
-});
+const toggle = document.getElementById("menu-toggle")
+const menu = document.getElementById("menu")
+
+toggle.onclick = () => {
+menu.classList.toggle("active")
+}
+
+window.addEventListener("scroll",function(){
+
+const header=document.getElementById("header")
+
+if(window.scrollY>50){
+
+header.classList.add("scrolled")
+
+}else{
+
+header.classList.remove("scrolled")
+
+}
+
+})
